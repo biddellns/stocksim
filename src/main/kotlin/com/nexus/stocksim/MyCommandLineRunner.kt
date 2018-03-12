34363547b91c05fa586f79c2bf37d0d8f102cbf6
Nicholas Biddell <nicbiddell@gmail.com>
2018-03-11 23:50:32 -0400

    Add command line runner

---
+++ src/main/kotlin/com/nexus/stocksim/MyCommandLineRunner.kt
@@ -0,0 +1,35 @@
+package com.nexus.stocksim
+
+import com.nexus.stocksim.domain.PriceQuote
+import com.nexus.stocksim.repository.PriceQuoteRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+import java.math.BigDecimal
+
+@Component
+class MyCommandLineRunner : CommandLineRunner {
+
+    @Autowired
+    private val priceQuoteRepository: PriceQuoteRepository? = null;
+
+    override fun run(vararg args: String) {
+        log.info(".......................")
+        log.info("Adding GOOG")
+
+        val goog = PriceQuote("GOOO", 50, BigDecimal(1.11))
+        priceQuoteRepository?.save(goog)
+        log.info("Added GOOG")
+
+        log.info("Finding all PriceQuotes")
+        priceQuoteRepository!!.findAll().forEach({ quote -> log.info(quote.toString()) })
+
+    }
+
+    companion object {
+        val log: Logger =
+                LoggerFactory.getLogger(MyCommandLineRunner::class.java)
+    }
+}
