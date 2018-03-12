9a1785524f657aadfc5fe5b2402290944e916448
Nicholas Biddell <nicbiddell@gmail.com>
2018-03-11 23:49:09 -0400

    Enable JPA auditing

--- src/main/kotlin/com/nexus/stocksim/StocksimApplication.kt
+++ src/main/kotlin/com/nexus/stocksim/StocksimApplication.kt
@@ -2,8 +2,10 @@ package com.nexus.stocksim
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class StocksimApplication
 
 fun main(args: Array<String>) {
